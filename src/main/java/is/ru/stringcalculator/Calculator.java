ba933243c16796e00617dd7874aec92c61b89289
sonjas12 <sonjasteinarsdottir@gmail.com>
2014-10-17 23:24:33 +0000

    add new line function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
 		}
 		else
@@ -18,16 +18,17 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	   	 numbers = numbers.replace("\n",",");
+		 return numbers.split(",");
 	}
       
-    private static int sum(String[] numbers){
+	 private static int sum(String[] numbers){
  	    int total = 0;
-        for(String number : numbers){
+       		 for(String number : numbers){
 		    total += toInt(number);
 		}
 		return total;
-    }
+    	}
 
 
 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.Calculator");
     }
 
 	@Test
@@ -25,8 +25,12 @@ public class CalculatorTest {
 	}	
 
 	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
+   	 public void testMultipleNumbers(){
+    		assertEquals(6, Calculator.add("1,2,3"));
+   	 }
+	@Test
+	public void testNewLine(){
+		assertEquals(6, Calculator.add("1\n2,3"));	
 
+	}
 }
